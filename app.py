9c3497e8d15ddfb7e634d2a2fb900b415b6e46e4
Catataclop <lise.aymonin@gmail.com>
2025-10-21 09:38:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 st.title("Bonjourre à tousses")
 st.header("Bienvenue dans mon app à moi")
 st.subheader("Elle est pas copiée sur Romain nonnon")
+st.subheader("Regardez c'est pas pareil : https://mon-apply.streamlit.app/")
 
 st.write("Sans transition, voici un data frame de nombres et leurs carrés :")
 nmb = [1,2,4,7]
