09234756fb9444b1b828d6f12835980f468eaf38
Catataclop <lise.aymonin@gmail.com>
2025-10-21 09:31:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import streamlit as st
 
 st.header("C'est mon app à moi")
 st.write("J'ai pas copié Romain nonnon")
+
+nmb = [1,2,4,7]
+carre = [1**2,2**2,4**2,7**2]
+d = {"nombres":nmb, "carres":carre} # création d'un bibliothèque
+
+data = pd.DataFrame(d) # dataframe associé avec pandas
+
+st.dataFrame(data) # affichage du dataframe dans streamlit
